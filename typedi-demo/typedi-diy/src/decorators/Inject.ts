a1c0c412b123a92f440aa059786d43a2686c6120
linbudu599 <linbudu599@gmail.com>
2020-07-06 22:10:33 +0800

    :whale: chore: simplify injector decorator

--- typedi-demo/typedi-diy/src/decorators/Inject.ts
+++ typedi-demo/typedi-diy/src/decorators/Inject.ts
@@ -1,26 +1,13 @@
 import { Container } from "../Container";
 
-export function Inject(
-  typeOrName?: ((type?: any) => Function) | string
-): Function {
+export function Inject(typeOrName): PropertyDecorator {
   return function (target: Object, propertyName: string, index?: number) {
-    if (!typeOrName)
-      typeOrName = () =>
-        (Reflect as any).getMetadata("design:type", target, propertyName);
-
     Container.registerHandler({
       object: target,
-      propertyName: propertyName,
-      index: index,
+      propertyName,
+      index,
       value: (containerInstance) => {
-        let identifier: any;
-        if (typeof typeOrName === "string") {
-          identifier = typeOrName;
-        } else {
-          identifier = typeOrName();
-        }
-
-        return containerInstance.get(identifier);
+        return containerInstance.get(typeOrName);
       },
     });
   };
--- typedi-demo/typedi-diy/src/decorators/Service.ts
+++ typedi-demo/typedi-diy/src/decorators/Service.ts
@@ -1,30 +1,10 @@
 import { Container } from "../Container";
-import { ContainerInstance } from "../ContainerInstance";
 
-export type ObjectType<T1> = { new (...args: any[]): T1 } | { service: T1 };
-
-export function Service(
-  optionsOrServiceName?: string | any[] | (() => any),
-  maybeFactory?: (...args: any[]) => any
-): any {
+export function Service(): any {
   return function (target: Function) {
     const service: any = {
       type: target,
     };
-
-    if (typeof optionsOrServiceName === "string") {
-      service.id = optionsOrServiceName;
-      service.multiple = (optionsOrServiceName as any).multiple;
-      service.global = (optionsOrServiceName as any).global || false;
-      service.transient = (optionsOrServiceName as any).transient;
-    } else if (optionsOrServiceName) {
-      service.id = (optionsOrServiceName as any).id;
-      service.factory = (optionsOrServiceName as any).factory;
-      service.multiple = (optionsOrServiceName as any).multiple;
-      service.global = (optionsOrServiceName as any).global || false;
-      service.transient = (optionsOrServiceName as any).transient;
-    }
-
     Container.set(service);
   };
 }
